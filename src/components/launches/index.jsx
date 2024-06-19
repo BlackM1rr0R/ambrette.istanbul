25f73ed1d136a4ba5e6c9abe9ed08b18a0b9d930
7u4bgr <abdllyevv@gmail.com>
2024-06-20 03:09:17 +0400

    add see more buttons

--- src/components/launches/index.jsx
+++ src/components/launches/index.jsx
@@ -3,7 +3,11 @@ import styles from "./index.module.css";
 import Wrapper from "../UI/wrapper";
 import { Link } from "react-router-dom";
 import DB from "../../db.json";
+
 const LaunchesPerfume = () => {
+  const [more, setMore] = useState(false);
+  const [moreRandom, setMoreRandom] = useState(false);
+
   const uniqueBrands = [...new Set(DB.map((product) => product.brands))];
   const capitalizeWords = (str) => {
     return str
@@ -11,6 +15,7 @@ const LaunchesPerfume = () => {
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
       .join(" ");
   };
+
   const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -18,8 +23,10 @@ const LaunchesPerfume = () => {
     }
     return array;
   };
+
   const shuffledDB = shuffleArray([...DB]);
   const randomBrands = shuffledDB.slice(0, 25);
+
   return (
     <Wrapper>
       <div className={styles.background}>
@@ -36,17 +43,23 @@ const LaunchesPerfume = () => {
         </div>
         <div className={styles.perfumelist1}>
           <div className={styles.firstside1}>
-          <ul>
-              {randomBrands.slice(0, 25).map((brand, index) => (
+            <ul>
+              {randomBrands.slice(0, moreRandom ? randomBrands.length : 12).map((brand, index) => (
                 <li key={index}>
-                  <Link
-                    to={`/parfum-details/${brand.id}`}
-                  >
+                  <Link to={`/parfum-details/${brand.id}`}>
                     {capitalizeWords(brand.title)}
                   </Link>
                 </li>
               ))}
             </ul>
+          
+              <div className={styles.button}>
+
+              <button onClick={() => setMoreRandom(!moreRandom)} className={styles.seeMore}>
+              {moreRandom ? "See Less" : "See More"}
+              </button>
+              </div>
+          
           </div>
         </div>
         <div className={styles.headers}>
@@ -54,7 +67,7 @@ const LaunchesPerfume = () => {
             <hr />
           </div>
           <div className={styles.h2}>
-            <h2>Most Popular Brends</h2>
+            <h2>Most Popular Brands</h2>
           </div>
           <div className={styles.hr}>
             <hr />
@@ -63,16 +76,22 @@ const LaunchesPerfume = () => {
         <div className={styles.perfumelist}>
           <div className={styles.firstside}>
             <ul>
-              {uniqueBrands.slice(0, 48).map((brand, index) => (
+            {uniqueBrands.slice(0, more ? 48 : 18).map((brand, index) => (
                 <li key={index}>
-                  <Link
-                    to={`/brend/${brand.replace(/\s+/g, "").toLowerCase()}`}
-                  >
+                  <Link to={`/brand/${brand.replace(/\s+/g, "").toLowerCase()}`}>
                     {capitalizeWords(brand)}
                   </Link>
                 </li>
               ))}
             </ul>
+      
+              <div className={styles.button}>
+
+              <button onClick={() => setMore(!more)} className={styles.seeMore}>
+              {more ? "See Less" : "See More"}
+              </button>
+              </div>
+         
           </div>
         </div>
       </div>
